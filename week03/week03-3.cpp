b14b93baf05ce75d027c763bce1ac9887ee78d6b
mayo <91170816+mayo0112@users.noreply.github.com>
2021-12-15 11:22:13 +0800

    Add files via upload

---
+++ week03/week03-3.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf( "%a%b", &a, &b );
+    printf( "%d", a+b );
+}
---
+++ week03/week03-4.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf( "Hollow World\n");
+    printf( "%d" , 300 );
+}

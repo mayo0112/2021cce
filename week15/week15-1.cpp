4cb36f4fa0a63909e77ef113e6d5e958576ef2a3
mayo <91170816+mayo0112@users.noreply.github.com>
2021-12-22 11:58:44 +0800

    Add files via upload

---
+++ week15/week15-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,ans=1;
+    scanf("%d %d",&a,&b);
+    for(int i=1;i<=a;i++)
+    {
+        if(a%i==0 && b%i==0) ans=i;
+    }
+    printf("%d",ans);
+}
